579d74ff4f2dee29f5c6634547c390224644535d
unknown <bankurupavansai2007@gmail.com>
2025-09-13 17:19:47 +0530

    login

--- npm project/src/App.jsx
+++ npm project/src/App.jsx
@@ -1,28 +1,60 @@
-import "./ProductCard.css"
-import BlogPost from "./BlogPost.jsx"
-import Idcard from "./Idcard.jsx"
-import ProductCard from "./ProductCard.jsx"
+import "./App.css"
+import { useState } from "react"
 function App() {
-  const posts = [
-    { id:1,author: "pavan", title: "return of dragon", description: "how dragon fly" },
-    { id:2,author: "sai", title: "going to hell", description: "why you want to go hell" },
-    { id:3,author: "pavansai", title: "going to heaven", description: "why should you want to go heaven" }
-  ]
+  const [formdata, setformdata] = useState({ email: "", password: "" })
 
-  return (
-    <div className="container">
+  const handleChange = (event) => {
+    setformdata(
       {
-        posts.map((post) => (
-          <BlogPost
-            key={post.id}
-            author={post.author}
-            title={post.title}
-            description={post.description}
-          />
-        ))
+        ...formdata,
+        [event.target.name]: event.target.value
       }
+    )
+  }
+
+  const [count, setCount] = useState(0)
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const handleEmail = (event) => {
+    setEmail(event.target.value)
+  }
+  const handlePassword = (event) => {
+    setPassword(event.target.value)
+  }
+  const handleInc = () => {
+    setCount((prevCount) => prevCount + 1)
+    console.log(count)
+  }
+  /*const handleChange = (event) => {
+    console.log(event.target.name)
+    console.log(event.target.value)
+  }*/
+  const handleClick = (name) => {
+
+    console.log("login successful")
+    alert("you clicked the button")
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert('Email: ' + formdata.email + ' Password: ' + formdata.password);
+
+  }
+  return (
+    <div>
+      <p>I am a button</p>
+      <form onSubmit={handleSubmit}></form>
+      <p>{count}</p>
+      <button onClick={handleInc}>Increment</button>
+      <input type="text" name="email" placeholder="Enter your Email" onChange={handleChange} />
+      <input type="text" name="password" placeholder="Enter your Password" onChange={handleChange} />
+      <button type="submit" onClick={handleSubmit}>login</button>
+      
+
+
+
     </div>
   )
 }
+
 
 export default App
